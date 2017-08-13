5fa1154753ae6da3a3599681612b48618b6509ca
kaschmo <schmolders@gmail.com>
2017-08-13 17:10:35 +0200

    Added OTA, prepared for Sonoff. All baseline functions aboard.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # ir_hub_git
-ESP8266 based IR hub to connect to openHAB
+ESP8266 based IR hub to connect to openHAB. 
+Designed for Sonoff Relay
+
+Sonoff PINs from reset button downwards
+1. VCC
+2. RX = IO 3 = SDA
+3. TX = IO 1 = SCL
+4. GND
+5. IO 14 = IR
--- ir_hub_git.ino
+++ ir_hub_git.ino
@@ -11,14 +11,22 @@
 #include <ir_Argo.h> 
 //required for MQTT
 #include <ESP8266WiFi.h>
+//required for OTA updater
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+#include <ESP8266HTTPUpdateServer.h>
+//end OTA requirements
 #include <PubSubClient.h>
 #include <Adafruit_BME280.h>
 
 
-// An IR detector/demodulator is connected to GPIO pin 14(D5 on a NodeMCU
-uint16_t RECV_PIN = 14;
-IRArgoESP argoir(D1);
-IRrecv irrecv(RECV_PIN);
+// An IR detector/demodulator is connected to GPIO pin 14 on Sonoff
+uint16_t IR_SEND_PIN = 14;
+uint16_t BME_SCL = 1;
+uint16_t BME_SDA = 3;
+IRArgoESP argoir(IR_SEND_PIN);
+//IRrecv irrecv(RECV_PIN); //Receiver 
 decode_results results;  // Somewhere to store the results
 irparams_t save;         // A place to copy the interrupt state while decoding.
 
@@ -36,12 +44,16 @@ PubSubClient client(espClient);
 
 const char* inTopic = "cmnd/ac_argo/#";
 const char* outTopic = "stat/ac_argo/";
+const char* mqtt_id = "ac_argo";
 
 //BME280
 #define SEALEVELPRESSURE_HPA (1013.25)
 Adafruit_BME280 bme280; // I2C
 float bme280_temperature, bme280_pressure, bme280_humidity, bme280_height;
 
+//OTA
+ESP8266WebServer httpServer(80);
+ESP8266HTTPUpdateServer httpUpdater;
 
 void setup_wifi() {
   delay(10);
@@ -62,6 +74,9 @@ void setup_wifi() {
   Serial.println("WiFi connected");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
+
+  httpUpdater.setup(&httpServer);
+  httpServer.begin();
 }
 
 
@@ -224,18 +239,26 @@ void sendACStatus(){
    dtostrf(bme280_pressure,2,2,msg); 
    strcat(outTopic_status,"pressure");
    client.publish(outTopic_status, msg);
-   
+
+    //IP Address
+   strcpy(outTopic_status,outTopic);
+   strcat(outTopic_status,"pressure");
+   WiFi.localIP().toString().toCharArray(msg,50);
+   client.publish(outTopic_status,msg ); 
+
 }
 
 void reconnect() {
   // Loop until we're reconnected
+  
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
     // Attempt to connect
-    if (client.connect("ESP8266Client")) {
+    if (client.connect(mqtt_id)) {
       Serial.println("connected");
       
-      client.publish(outTopic, "AC Hub booted");
+      client.publish(outTopic, "ac_argo booted");
+      
       //send current Status via MQTT to world
       sendACStatus();
       // ... and resubscribe
@@ -263,7 +286,7 @@ void update_sensors() {
 void setup() {
   // Status message will be sent to the PC at 115200 baud
   Serial.begin(115200, SERIAL_8N1, SERIAL_TX_ONLY);
-  irrecv.enableIRIn();  // Start the receiver
+  
   argoir.begin();
 
   //INIT TIMERS
@@ -272,14 +295,14 @@ void setup() {
 
   //INIT BME280
   //SDA, SCL
-  Wire.begin(D3, D4);
+  Wire.begin(BME_SDA, BME_SCL);
   bool status;
   status = bme280.begin();
   if (!status) {
       Serial.println("Could not find a valid BME280 sensor, check wiring!");
       while (1);
   }
-  update_sensors();
+  update_sensors(); 
 
   //WIFI and MQTT
   setup_wifi();                   // Connect to wifi 
@@ -290,100 +313,18 @@ void setup() {
   printACStatus();  
 }
 
-// Dump out the decode_results structure.
-void dumpRaw(decode_results *results) {
-  // Print Raw data
-  Serial.print("Timing[");
-  Serial.print(results->rawlen - 1, DEC);
-  Serial.println("]: ");
-
-  for (uint16_t i = 1;  i < results->rawlen;  i++) {
-    if (i % 100 == 0)
-      yield();  // Preemptive yield every 100th entry to feed the WDT.
-    uint32_t x = results->rawbuf[i] * USECPERTICK;
-    if (!(i & 1)) {  // even
-      Serial.print("-");
-      if (x < 1000) Serial.print(" ");
-      if (x < 100) Serial.print(" ");
-      Serial.print(x, DEC);
-    } else {  // odd
-      Serial.print("     ");
-      Serial.print("+");
-      if (x < 1000) Serial.print(" ");
-      if (x < 100) Serial.print(" ");
-      Serial.print(x, DEC);
-      if (i < results->rawlen - 1)
-        Serial.print(", ");  // ',' not needed for last one
-    }
-    if (!(i % 8)) Serial.println("");
-  }
-  Serial.println("");  // Newline
-}
-
-// Dump out the decode_results structure.
-//
-void dumpCode(decode_results *results) {
-  // Start declaration
-  Serial.print("uint16_t  ");              // variable type
-  Serial.print("rawData[");                // array name
-  Serial.print(results->rawlen - 1, DEC);  // array size
-  Serial.print("] = {");                   // Start declaration
-
-  // Dump data
-  for (uint16_t i = 1; i < results->rawlen; i++) {
-    Serial.print(results->rawbuf[i] * USECPERTICK, DEC);
-    if (i < results->rawlen - 1)
-      Serial.print(",");  // ',' not needed on last one
-    if (!(i & 1)) Serial.print(" ");
-  }
-
-  // End declaration
-  Serial.print("};");  //
-
-  // Comment
-  
-  Serial.print(" ");
-  serialPrintUint64(results->value, 16);
-
-  // Newline
-  Serial.println("");
-
-  // Now dump "known" codes
-  if (results->decode_type != UNKNOWN) {
-    // Some protocols have an address &/or command.
-    // NOTE: It will ignore the atypical case when a message has been decoded
-    // but the address & the command are both 0.
-    if (results->address > 0 || results->command > 0) {
-      Serial.print("uint32_t  address = 0x");
-      Serial.print(results->address, HEX);
-      Serial.println(";");
-      Serial.print("uint32_t  command = 0x");
-      Serial.print(results->command, HEX);
-      Serial.println(";");
-    }
-
-    // All protocols have data
-    Serial.print("uint64_t  data = 0x");
-    serialPrintUint64(results->value, 16);
-    Serial.println(";");
-  }
-}
 
 void loop() {
   if (!client.connected()) {
     reconnect();
   }
   client.loop();
-  // Check if the IR code has been received.
-  if (irrecv.decode(&results, &save)) {
-    Serial.println("Receiving IR data");
-    //dumpRaw(&results);            // Output the results in RAW format
-    dumpCode(&results);           // Output the results as source code
-    Serial.println("");           // Blank line between entries
-  }
 
   update_sensors();
 
+
+  //http Updater for OTA
+  httpServer.handleClient(); 
 
   //send status update via MQTT every minute
   if(millis()-timer_update_state_count > timer_update_state) {
